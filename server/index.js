506c7543e4ea9a14076b2822320970ca59968b12
Pollock Nag <75268620+Pollock-Nag@users.noreply.github.com>
2023-05-04 15:07:09 +0600

    added folder structure

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3001;
-// const connect = require('./models/db.connection');
+const connect = require('./db/connection');
 require('dotenv').config();
 
 // app.use(bodyParser.json({limit:'50mb'})) // for uploading large file in cloudinary
