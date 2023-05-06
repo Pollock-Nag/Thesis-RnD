1c7274ad3f8e6e125a5e0abbebb85a96b7b116ed
Zahid Ul Islam <zahidtwt@gmail.com>
2023-05-06 09:45:54 +0600

    skills controller

--- server/controllers/skill.controller.js
+++ server/controllers/skill.controller.js
@@ -2,8 +2,11 @@ const Skill = require('../models/student/skill.model');
 
 const addSkill = async (req, res) => {
   try {
-    const { skillName, description, marks } = req.body;
-    const skill = new Skill({ skillName, description, marks });
+    const { skillName, description, category } = req.body;
+
+    const skill = new Skill({ skillName, description });
+    const categories = category.split(',').map((c) => c.trim());
+    skill.category.push(...categories);
     await skill.save();
     res.status(201).json(skill);
   } catch (error) {
@@ -12,6 +15,38 @@ const addSkill = async (req, res) => {
   }
 };
 
+const getAllSkills = async (req, res) => {
+  console.log('hello');
+  try {
+    const skills = await Skill.find();
+    res.status(200).json(skills);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+const getJuniorSkills = async (req, res) => {
+  try {
+    const skills = await Skill.find({ category: 'junior' });
+    res.status(200).json(skills);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+const getSeniorSkills = async (req, res) => {
+  try {
+    const skills = await Skill.find({ category: 'senior' });
+    res.status(200).json(skills);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 module.exports = {
   addSkill,
+  getAllSkills,
+  getJuniorSkills,
+  getSeniorSkills,
 };
--- server/routes/skill.route.js
+++ server/routes/skill.route.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const skillControllers = require('../controllers/skill.controller');
 
 router.post('/addSkill', skillControllers.addSkill);
+router.get('/getAllSkills', skillControllers.getAllSkills);
+router.get('/getJuniorSkills', skillControllers.getJuniorSkills);
+router.get('/getSeniorSkills', skillControllers.getSeniorSkills);
 
 module.exports = router;
